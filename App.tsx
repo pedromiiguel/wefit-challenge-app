95f54f82143056cfc4a6ae85e42c31ff07cefd9b
Pedro Miguel <pedromrap@gmail.com>
2024-05-31 22:49:23 -0300

    chore: add react-native-async-store, zustand and gorhom/bottom-sheet

--- App.tsx
+++ App.tsx
@@ -1,6 +1,8 @@
 import { config } from '@gluestack-ui/config';
 import { GluestackUIProvider } from '@gluestack-ui/themed';
+import { BottomSheetModalProvider } from '@gorhom/bottom-sheet';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
 
 import { Routes } from './src/routes/routes';
 const queryClient = new QueryClient();
@@ -8,7 +10,11 @@ export default function App() {
   return (
     <GluestackUIProvider config={config}>
       <QueryClientProvider client={queryClient}>
-        <Routes />
+        <GestureHandlerRootView style={{ flex: 1 }}>
+          <BottomSheetModalProvider>
+            <Routes />
+          </BottomSheetModalProvider>
+        </GestureHandlerRootView>
       </QueryClientProvider>
     </GluestackUIProvider>
   );
