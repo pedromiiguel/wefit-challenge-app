dc32bdcb42ca38621aa4f1134fa79b0f7e99454d
Pedro Miguel <pedromrap@gmail.com>
2024-05-31 22:52:44 -0300

    feat: implement persistent favorites store using zustand

---
+++ src/services/favorites/useFavoritesStore.ts
@@ -0,0 +1,41 @@
+import { create } from 'zustand';
+import { persist } from 'zustand/middleware';
+
+import { GithubRepository } from '../github/githubTypes';
+import { asyncStorage } from '../storage';
+type FavoritesStore = {
+  items: GithubRepository[];
+
+  addToFavorites: (item: GithubRepository) => void;
+  removeFromFavorites: (id: number) => void;
+};
+
+const STORAGE_KEY = '@wefit-challenge-favorites';
+
+const useFavoritesStore = create<FavoritesStore>()(
+  persist(
+    set => ({
+      items: [],
+      selected: null,
+      addToFavorites: item => set(state => ({ items: [...state.items, item] })),
+      removeFromFavorites: id =>
+        set(state => ({
+          items: state.items.filter(item => item.id !== id),
+        })),
+    }),
+    {
+      name: STORAGE_KEY,
+      storage: asyncStorage,
+    },
+  ),
+);
+
+export function useFavorites() {
+  const addToFavorites = useFavoritesStore(state => state.addToFavorites);
+  const removeFromFavorites = useFavoritesStore(
+    state => state.removeFromFavorites,
+  );
+  const favorites = useFavoritesStore(state => state.items);
+
+  return { addToFavorites, removeFromFavorites, favorites };
+}
