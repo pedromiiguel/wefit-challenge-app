f8760a14b5e7132b1e77970a99fc16f673550df6
Pedro Miguel <pedromrap@gmail.com>
2024-05-31 22:43:44 -0300

    feat: add new routes on app stack

--- src/routes/app.stack.tsx
+++ src/routes/app.stack.tsx
@@ -1,12 +1,25 @@
 import { NavigatorScreenParams } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+import { DetailsScreen, WebViewScreen } from '@screens';
+
 import { AppTabBottomTabParamList, TabRoutes } from './tab.routes';
 
-export type RootStackParamList = {
+export type AppStackParamList = {
   AppTabNavigator: NavigatorScreenParams<AppTabBottomTabParamList>;
+  DetailsScreen: {
+    id: number;
+    fullName: string;
+    description: string;
+    htmlUrl: string;
+    language: string;
+  };
+  WebViewScreen: {
+    url: string;
+  };
 };
 
-const Stack = createNativeStackNavigator<RootStackParamList>();
+const Stack = createNativeStackNavigator<AppStackParamList>();
 
 export function AppStack() {
   return (
@@ -17,6 +30,8 @@ export function AppStack() {
       }}
       initialRouteName="AppTabNavigator">
       <Stack.Screen name="AppTabNavigator" component={TabRoutes} />
+      <Stack.Screen name="DetailsScreen" component={DetailsScreen} />
+      <Stack.Screen name="WebViewScreen" component={WebViewScreen} />
     </Stack.Navigator>
   );
 }
