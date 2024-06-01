3672acebd4a6d0bb19ed6eb94822441ceb47522e
Pedro Miguel <pedromrap@gmail.com>
2024-05-31 22:47:49 -0300

    chore: add axios and react query

--- App.tsx
+++ App.tsx
@@ -1,10 +1,15 @@
 import { config } from '@gluestack-ui/config';
 import { GluestackUIProvider } from '@gluestack-ui/themed';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
 import { Routes } from './src/routes/routes';
+const queryClient = new QueryClient();
 export default function App() {
   return (
     <GluestackUIProvider config={config}>
-      <Routes />
+      <QueryClientProvider client={queryClient}>
+        <Routes />
+      </QueryClientProvider>
     </GluestackUIProvider>
   );
 }
---
+++ src/api/apiConfig.ts
@@ -0,0 +1,7 @@
+import axios from 'axios';
+
+export const BASE_URL = process.env.EXPO_PUBLIC_BASE_URL;
+
+export const api = axios.create({
+  baseURL: BASE_URL || 'https://api.github.com/',
+});
