d0c6fc13f367b6b630adea7dabfe5f267a633050
Pedro Miguel <pedromrap@gmail.com>
2024-05-31 22:38:34 -0300

    feat: add empty list component

---
+++ src/components/EmptyList/index.tsx
@@ -0,0 +1,44 @@
+import React from 'react';
+
+import { Box, Button, ButtonText, Text } from '@gluestack-ui/themed';
+
+export interface EmptyListProps {
+  loading: boolean;
+  error: unknown;
+  refetch: () => void;
+  emptyMessage?: string;
+  errorMessage?: string;
+}
+export function EmptyList({
+  loading,
+  error,
+  refetch,
+  emptyMessage = 'Não encontramos nenhum repositorio.',
+  errorMessage = 'Não foi possível carregar os repositórios.',
+}: EmptyListProps) {
+  if (loading) {
+    return null;
+  }
+
+  let component = <Text fontWeight={'$semibold'}>{emptyMessage}</Text>;
+
+  if (error) {
+    component = (
+      <Box gap="$4">
+        <Text fontWeight={'$semibold'} color="$red10">
+          {errorMessage}
+        </Text>
+
+        <Button onPress={refetch} bg="#1976D2">
+          <ButtonText>Recarregar</ButtonText>
+        </Button>
+      </Box>
+    );
+  }
+
+  return (
+    <Box flex={1} justifyContent="center" alignItems="center">
+      {component}
+    </Box>
+  );
+}
