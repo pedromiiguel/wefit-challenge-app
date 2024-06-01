cae9cf10c71c94b1c76501681e0f514208f03d7b
Pedro Miguel <pedromrap@gmail.com>
2024-05-31 22:44:07 -0300

    feat: define and export navigation prop types for App stack and tab navigators

---
+++ src/routes/index.ts
@@ -0,0 +1,6 @@
+export * from './routes';
+export * from './nagivationType';
+export * from './tab.routes';
+export * from './app.stack';
+export * from './mapScreenToProps';
+export * from './AppTabBar';
